861e0e99fd73525b03729f3a0a634fa2c71ea43d
Atwijukire Ariho Seth <arihoseth18@gmail.com>
2025-05-05 22:21:57 +0100

    Before adding the Email API

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,8 +1,8 @@
-import { Facebook, Github, Instagram, Linkedin, Mail, MapPin, Phone, Twitter, X, Youtube } from "lucide-react";
+import { Facebook, Github, Instagram, Linkedin, Mail, MapPin, Phone, Send, Twitter, X, Youtube } from "lucide-react";
 import { cn } from "../lib/utils";
 
-const ContactSection=()=>{
-    return(
+const ContactSection = () => {
+    return (
         <section id="contacts" className="py-24-px-4 relative bg-secondary/30">
             <div className="container mx-auto max-w-5xl">
                 <h2 className="text-3xl md:texl-4xl font-bold mb-4 text-center">
@@ -10,7 +10,7 @@ const ContactSection=()=>{
                 </h2>
                 <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
                     Have a project in mind that you want me to visualize?, Feel free to reach out to me
-                    Am always ready to discuss new opportunuties.
+                    I'm always ready to discuss new opportunuties.
                 </p>
 
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
@@ -21,8 +21,8 @@ const ContactSection=()=>{
                                 <div className="p-3 rounded-full bg-primary/10"> <Mail /> </div>
                                 <div>
                                     <h4 className="font-medium "> Email</h4>
-                                     <a href="mailto:arihoseth@gmail.com" 
-                                     className="text-muted-foreground hover:text-primary transition-colors">arihoseth@gmail.com</a> 
+                                    <a href="mailto:arihoseth@gmail.com"
+                                        className="text-muted-foreground hover:text-primary transition-colors">arihoseth@gmail.com</a>
                                 </div>
                             </div>
                         </div>
@@ -32,8 +32,8 @@ const ContactSection=()=>{
                                 <div className="p-3 rounded-full bg-primary/10"> <Phone /> </div>
                                 <div>
                                     <h4 className="font-medium "> Phone </h4>
-                                     <a href="tel:+11489443763" 
-                                     className="text-muted-foreground hover:text-primary transition-colors">+11489443763</a> 
+                                    <a href="tel:+11489443763"
+                                        className="text-muted-foreground hover:text-primary transition-colors">+11489443763</a>
                                 </div>
                             </div>
                         </div>
@@ -43,64 +43,90 @@ const ContactSection=()=>{
                                 <div className="p-3 rounded-full bg-primary/10"> <MapPin /> </div>
                                 <div>
                                     <h4 className="font-medium ">Location</h4>
-                                     <a  
-                                     className="text-muted-foreground hover:text-primary transition-colors">Boumerdes, Algeria</a> 
+                                    <a
+                                        className="text-muted-foreground hover:text-primary transition-colors">Boumerdes, Algeria</a>
                                 </div>
                             </div>
 
                             <div className="pt-8">
-                            <h4 className="font-medium mb-4">Connect with Me</h4>
-                            <div className="flex space-x-4 justify-center">
-                                <a href="https://www.linkedin.com/in/atwijukire-ariho-seth-4a7a38259/" target="_blank">
-                                    <Linkedin />
-                                </a>
-                                <a href="https://www.instagram.com/ariho_seth/" target="_blank">
-                                    <Instagram />
-                                </a>
-                                <a href="https://www.facebook.com/ArihoSeth18/" target="_blank">
-                                    <Facebook />
-                                </a>
-                                <a href="https://x.com/ariho_seth18" target="_blank">
-                                    <Twitter />
-                                   
-                                </a>
-                                <a href="https://www.youtube.com/@ArihoSeth" target="_blank">
-                                    <Youtube />
-                                </a>
-                                <a href="https://github.com/Ariho-Seth" target="_blank">
-                                    <Github />
-                                </a>
+                                <h4 className="font-medium mb-4">Connect with Me</h4>
+                                <div className="flex space-x-4 justify-center">
+                                    <a href="https://www.linkedin.com/in/atwijukire-ariho-seth-4a7a38259/" target="_blank">
+                                        <Linkedin />
+                                    </a>
+                                    <a href="https://www.instagram.com/ariho_seth/" target="_blank">
+                                        <Instagram />
+                                    </a>
+                                    <a href="https://www.facebook.com/ArihoSeth18/" target="_blank">
+                                        <Facebook />
+                                    </a>
+                                    <a href="https://x.com/ariho_seth18" target="_blank">
+                                        <Twitter />
+
+                                    </a>
+                                    <a href="https://www.youtube.com/@ArihoSeth" target="_blank">
+                                        <Youtube />
+                                    </a>
+                                    <a href="https://github.com/Ariho-Seth" target="_blank">
+                                        <Github />
+                                    </a>
+                                </div>
                             </div>
-                    </div>  
 
 
                         </div>
 
-                        
 
 
-                    
+
+
                     </div>
 
-                    
+
 
                     <div className="bg-card p-8 rounded-lg shadow-xs">
                         <h3 className="text-2xl font-semibold mb-6">Send a Message</h3>
 
                         <form className="space-y-6">
                             <div>
-                                <label htmlFor="name">Your Name</label>
-                                <input type="text" 
-                                id="name" name="name" 
-                                required 
-                                className={cn("w-full px-4 py-3 rounded-md border border-input bg-backgroung",
-                                            "focus:outlind-hidden foucus:ring-2-color focus:ring-primary")} 
-                                placeholder="Ariho Seth..."/>
+                                <label htmlFor="name" className="block text-sm font-medium mb-2">Your Name</label>
+                                <input type="text"
+                                    id="name" name="name"
+                                    required
+                                    className={cn("w-full px-4 py-3 rounded-md border border-input bg-background",
+                                        "focus:outlind-hidden foucus:ring-2-color focus:ring-primary")}
+                                    placeholder="Ariho Seth..." />
                             </div>
+                            <div>
+                                <label htmlFor="name" className="block text-sm font-medium mb-2">Your Email</label>
+                                <input type="email"
+                                    id="email" name="email"
+                                    required
+                                    className={cn("w-full px-4 py-3 rounded-md border border-input bg-background",
+                                        "focus:outlind-hidden foucus:ring-2-color focus:ring-primary")}
+                                    placeholder="youremail@gmail.com..." />
+
+                            </div>
+
+
+                            <div>
+                                <label htmlFor="message" className="block text-sm font-medium mb-2">Your Message</label>
+                                <textarea type="text"
+                                    id="message" name="message"
+                                    required
+                                    className={cn("w-full px-4 py-3 rounded-md border border-input bg-background",
+                                        "focus:outlind-hidden foucus:ring-2-color focus:ring-primary resize-none")}
+                                    placeholder="Type your message here..." />
+                            </div>
+                            <button type="submit" className={cn("cosmic-button w-full flex items-center justify-center gap-2",
+                                                                
+
+                            )}> Send Message
+                                <Send />
+
+                            </button>
                         </form>
                     </div>
-
-                   
                 </div>
             </div>
         </section>
